0ef10b7364fb1664bf1d7986dbe0b8c242539c5f
Egor <Lixen1@protonmail.com>
2025-03-31 22:35:44 +0300

    first-commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/server"
+)
+
 func main() {
 
+	logger := log.New(os.Stdout, "INFO: ", log.LstdFlags)
+
+	s := server.NewServer(logger)
+
+	logger.Println("Серевер запущен")
+	err := s.HTTPServer.ListenAndServe()
+	if err != nil {
+		logger.Fatal("ошибка во время работы сервера: ", err)
+	}
 }
